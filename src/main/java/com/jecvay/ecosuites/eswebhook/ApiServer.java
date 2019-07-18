86b6c6982773b17c2e8b76aad609285f19cb2f06
Jecvay <ljwjiewei@gmail.com>
2019-07-18 20:46:44 +0800

    use sun httpserver as mc's receive port

--- src/main/java/com/jecvay/ecosuites/eswebhook/ApiServer.java
+++ src/main/java/com/jecvay/ecosuites/eswebhook/ApiServer.java
@@ -1,13 +1,134 @@
 package com.jecvay.ecosuites.eswebhook;
 
-import org.apache.commons.lang3.ObjectUtils;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpServer;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.scheduler.SpongeExecutorService;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+
+public class ApiServer {
+    private static final String HOSTNAME = "0.0.0.0";
+    private static final int PORT = 51015;
+    private static final int BACKLOG = 1;
+
+    private static final int NO_RESPONSE_LENGTH = -1;
+
+    private SpongeExecutorService minecraftExecutor = null;
+
+
+    static private ApiServer server = null;
+
+    private ApiServer(SpongeExecutorService minecraftExecutor) {
+        this.minecraftExecutor = minecraftExecutor;
+        try {
+            final HttpServer server = HttpServer.create(new InetSocketAddress(HOSTNAME, PORT), BACKLOG);
+            server.createContext("/", he -> {
+                try {
+                    final Headers headers = he.getResponseHeaders();
+                    final String requestMethod = he.getRequestMethod().toUpperCase();
+                    switch (requestMethod) {
+                        case "POST":
+                            BufferedReader in = new BufferedReader(new InputStreamReader(he.getRequestBody()));
+                            String responseBody = in.lines().collect(Collectors.joining());
+                            onReceive(responseBody);
+                            headers.set("Content-Type", "application/json; charset=UTF-8");
+                            final byte[] rawResponseBody = responseBody.getBytes(StandardCharsets.UTF_8);
+                            he.sendResponseHeaders(200, rawResponseBody.length);
+                            he.getResponseBody().write(rawResponseBody);
+                            break;
+                        default:
+                            headers.set("Allow", "GET,POST");
+                            he.sendResponseHeaders(405, -1);
+                            break;
+                    }
+                } finally {
+                    he.close();
+                }
+            });
+            System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+            server.start();
+            System.out.println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static <K, V> Map<K, V> parseToMap(String json,
+                                              Class<K> keyType,
+                                              Class<V> valueType) {
+        return JSON.parseObject(json,
+                new TypeReference<Map<K, V>>(keyType, valueType) {
+                });
+    }
+
+    private void sendToConsole(String text) {
+        minecraftExecutor.submit(() -> {
+            try {
+                Sponge.getServer().setBroadcastChannel(MessageChannel.TO_CONSOLE);
+                Sponge.getServer().getBroadcastChannel().send(Text.of(text));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void sendToAll(String text) {
+        minecraftExecutor.submit(() -> {
+            try {
+                Sponge.getServer().setBroadcastChannel(MessageChannel.TO_ALL);
+                Sponge.getServer().getBroadcastChannel().send(Text.of(text));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void onReceive(String receiveText) {
+
+        try {
+            // List<Map<String, Object>> justMap = JSON.parseObject(receiveText, new TypeReference<List<Map<String, Object>>>(){});
+
+            Map<String, Object> data = parseToMap(receiveText, String.class, Object.class);
+            String action = data.getOrDefault("action", "unknown").toString();
+            if (action.equals("chat")) {
+                String content = data.getOrDefault("content", "").toString();
+                String name = data.getOrDefault("name", "").toString();
+                if (content.length() > 0 && name.length() > 0) {
+                    sendToAll(String.format("[qq: %s] %s", name, content));
+                } else {
+                    sendToConsole(String.format("[Unknown Coolq Chat] %s", receiveText));
+                }
+            } else {
+                sendToConsole(String.format("[Unknown Coolq Data] %s", receiveText));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    static public ApiServer getServer(SpongeExecutorService minecraftExecutor) {
+        if (server == null) {
+            server = new ApiServer(minecraftExecutor);
+        } else {
+            // server = new ApiServer();
+        }
+        return server;
+    }
+}
+
+/*
 public class ApiServer {
 
     final static int port = 51015;
@@ -73,3 +194,4 @@ class ListenerThread extends Thread {
         }
     }
 }
+*/
--- src/main/java/com/jecvay/ecosuites/eswebhook/ESWebhook.java
+++ src/main/java/com/jecvay/ecosuites/eswebhook/ESWebhook.java
@@ -2,6 +2,8 @@ package com.jecvay.ecosuites.eswebhook;
 
 import com.google.inject.Inject;
 import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.game.GameReloadEvent;
@@ -9,6 +11,9 @@ import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.message.MessageChannelEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.scheduler.SpongeExecutorService;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
 
 @Plugin(
         id = "es-webhook",
@@ -23,19 +28,24 @@ public class ESWebhook {
     @Inject
     private Logger logger;
 
+    @Inject
+    private Game game;
+
     private ApiServer apiServer = null;
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
         ApiClient.logger = logger;
-        apiServer = ApiServer.getServer();
+        SpongeExecutorService minecraftExecutor = Sponge.getScheduler().createSyncExecutor(this);
+        apiServer = ApiServer.getServer(minecraftExecutor);
         logger.info("-------Start-------->" + ApiClient.sendServerStartMsg());
     }
 
     @Listener
     public void onServerReload(GameReloadEvent event) {
         // ApiClient.sendServerReloadMsg();
-        apiServer = ApiServer.getServer();
+        SpongeExecutorService minecraftExecutor = Sponge.getScheduler().createSyncExecutor(this);
+        apiServer = ApiServer.getServer(minecraftExecutor);
         logger.info("-------Reload-------->" + ApiClient.sendServerReloadMsg() + apiServer.toString());
     }
 
